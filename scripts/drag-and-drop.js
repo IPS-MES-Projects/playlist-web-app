a5467358ab031391ca4567753808b14ffad9328c
skpdu <skpdu198@gmail.com>
2022-02-06 21:03:00 +0000

    show music list cards when selected, change description and title

--- scripts/drag-and-drop.js
+++ scripts/drag-and-drop.js
@@ -1,9 +1,20 @@
 let playlistsElements = document.querySelectorAll("#playlists-container div");
 let recordPlayerElement = document.getElementById("drop");
+let selectedPlaylist;
 
+playlistsElements.forEach(elem =>{
+    elem.addEventListener('dragstart', dragStart);
+});
 recordPlayerElement.addEventListener("dragover", dragOver);
 recordPlayerElement.addEventListener("dragleave", dragLeave);
 recordPlayerElement.addEventListener("drop", drop);
+
+function dragStart(event){
+    selectedPlaylist = this.id;
+    // console.log(selectedPlaylist);
+}
+
+
 
 // Alternate between playing record or not
 //todo bug when already playing going over and out will remove playing in image
@@ -26,7 +37,28 @@ function drop(event) {
     let playlistButtonsElement = document.getElementById("playlist-buttons");
     let playlistDescriptionElement = document.getElementById("playlist-description");
     let songsContainertElement = document.getElementById("songs-container");
+    let songsListElement = document.getElementById('songs-list');
+
+    playlistButtonsElement.classList.remove('d-none');
+    songsContainertElement.classList.remove('d-none');
+
+    playlistTitleElement.textContent = "To be replaced";
+    playlistDescriptionElement.textContent = "To be replaced with values from the playlist object saved in LocalStorage";
+
+    // loadMusicList(songsListElement);
 }
+
+
+// todo function to load music list still needs localStorage
+// function loadMusicList(songsListElement){
+//     let musicList = localStorage.getItem("musicList");
+
+//     musicList.forEach(songsListElement =>{
+//         let displayListItem = document.createElement('li');
+//         displayListItem.textContent = musicList;
+//         songsListElement.append(displayListItem);
+//     });
+// }
 
 // function selectPlaylist(){
 //     playlistsElements.forEach(elem => {
